f4847c24fa73e11a93ca27730a0f5ae86076fea8
tonylampada <tonylampada@gmail.com>
2017-04-29 11:44:44 -0300

    hello world com nuxtjs

---
+++ pages/profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Hello {{user.name}}
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'GithubProfile',
+  asyncData (context) {
+    console.log('asyncData!')
+    return axios.get('https://api.github.com/users/tonylampada').then((response) => {
+        return {user: response.data}
+    });
+  },
+  data () {
+    return {
+      user: null,
+    };
+  },
+}
+</script>
