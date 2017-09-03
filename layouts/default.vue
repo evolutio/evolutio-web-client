bcb618e6502b87580448b9914cdc3f752ba4e2c9
tonylampada <tonylampada@gmail.com>
2017-09-03 11:17:10 -0300

    home mais responsiva

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,7 @@
       <v-toolbar-title @click="go_home()" class="toolbar-title">evolutio</v-toolbar-title>
       <v-toolbar-items>
         <!-- <v-btn flat dark ripple v-if="!logged_user" @click="open_login_dialog($event)">Login</v-btn> -->
+        <v-btn flat dark ripple :href="'abcde'">Login</v-btn>
         <v-menu v-if="logged_user" bottom left offset-y origin="top right" transition="v-slide-y-transition">
           <v-btn flat dark ripple slot="activator">
             {{logged_user.username}}
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,43 @@
 <template>
   <main>
-    <v-container fluid class="main-content">
+    <!-- <v-container fluid class="main-content"> -->
       <v-layout row wrap>
-        <v-flex xs6 class="pt-5">
+        <v-flex xs12 sm4 offset-sm2 class="px-2 pt-5">
           <h5 class="pt-5">Aprenda tudo o que você vai usar no mundo real</h5>
           <p>O conteúdo com a qualidade que você precisa, em um só lugar.</p>
         </v-flex>
-        <v-flex xs6>
-          <p class="text-xs-right"><img src="/images/minecraft.png" ></p>
-          
+        <v-flex xs12 sm4>
+          <p class="text-sm-right"><img src="/images/minecraft.png" ></p>
+        </v-flex>
+        <v-flex xs12 sm8 offset-sm2 d-flex>
+          <v-card id="course-list">
+            <v-toolbar class="light-blue">
+              <v-toolbar-title>Cursos</v-toolbar-title>
+            </v-toolbar>
+            <v-list three-line subheader>
+              <v-list-item v-for="course in courses" :key="course.code">
+                <v-list-tile avatar :router="true" :to="{name: 'curso-code', params:{code: course.code}}">
+                  <v-list-tile-avatar>
+                    <img v-if="course.icon" :src="course.icon">
+                    <v-icon v-if="!course.icon" class="grey white--text">folder</v-icon>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ course.name }}, com <strong>{{ course.teachers }}</strong></v-list-tile-title>
+                    <v-list-tile-sub-title>{{ course.description }}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list-item>
+            </v-list>
+          </v-card>
         </v-flex>
       </v-layout>
-
-
-      <v-card id="course-list">
-        <v-toolbar class="light-blue">
-          <v-toolbar-title>Cursos</v-toolbar-title>
-        </v-toolbar>
-        <v-list two-line subheader>
-          <v-list-item v-for="course in courses" :key="course.code">
-            <v-list-tile avatar :router="true" :to="{name: 'curso-code', params:{code: course.code}}">
-              <v-list-tile-avatar>
-                <img v-if="course.icon" :src="course.icon">
-                <v-icon v-if="!course.icon" class="grey white--text">folder</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>{{ course.name }}, com <strong>{{ course.teachers }}</strong></v-list-tile-title>
-                <v-list-tile-sub-title>{{ course.description }}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-container>
+    <!-- </v-container> -->
   </main>
 </template>
 
 <script>
 
 import AppApi from '~apijs'
-import FacebookComments from '~/components/FacebookComments.vue'
 
 export default {
   asyncData(ctx) {
@@ -54,15 +52,14 @@ export default {
   },
   methods: {
   },
-  components: {FacebookComments},
 }
 </script>
 
 <style scoped>
-  .title {
+  /*.title {
     padding-left: 20px;
   }
   .main-content{
     max-width: 900px
-  }
+  }*/
 </style>
