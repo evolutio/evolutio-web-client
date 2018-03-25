d8e30ee4bb6ee5860f43145c50094905252f5a7a
tonylampada <tonylampada@gmail.com>
2018-03-25 11:03:28 -0300

    puta que o pariu como eu queria so um pouquinho de tempo pra faer as coisas....

--- components/Comments.vue
+++ components/Comments.vue
@@ -9,9 +9,9 @@
       </v-flex>
       <v-flex xs10 sm11>
         <v-text-field v-model="text" auto-grow rows="2" multi-line label="Adicione um comentário" 
-          @focus="editing=true" @blur="editing=false" @keyup.ctrl.enter="send"
+          @focus="editing=true" @blur="losefocus" @keyup.ctrl.enter="send"
            hint="Ctrl+Enter para enviar" persistent-hint></v-text-field>
-        <p v-if="editing" class="text-xs-right"><v-btn @click="send" :loading="sending" primary>Enviar</v-btn></p>
+        <p v-if="editing || sending" class="text-xs-right"><v-btn @click="send" :loading="sending" primary>Enviar</v-btn></p>
       </v-flex>
     </template>
     <template v-if="!loading" v-for="comment in comments">
@@ -23,7 +23,7 @@
       <v-flex xs10 sm11 class="grey lighten-2 mt-3 px-3 pt-3 pb-1 rounded">
         <p ><strong>{{comment.author}}</strong>, <span v-tooltip:top="{ html: comment.created_at }">{{comment.created_at}}</span></p>
         <p class="mb-1">{{comment.text}}</p>
-        <p class="mb-1"><a class="link" @click="openreply">Responder</a></p>
+        <p class="mb-1"><a class="link" @click="openreply(comment)">Responder</a></p>
       </v-flex>
       <template v-for="(reply, rindex) in comment.replies">
         <v-flex xs1></v-flex>
@@ -35,10 +35,26 @@
         <v-flex xs9 sm10 class="grey lighten-2 mt-3 px-3 pt-3 pb-1 rounded">
           <p><strong>{{reply.author}}</strong>, <span>{{reply.created_at}}</span></p>
           <p class="mb-1">{{reply.text}}</p>
-          <p v-if="rindex == comment.replies.length-1"><a class="link" @click="openreply">Responder</a></p>
+          <p v-if="rindex == comment.replies.length-1"><a class="link" @click="openreply(comment)">Responder</a></p>
+        </v-flex>
+      </template>
+      <template v-if="comment.showreply">
+        <v-flex xs1></v-flex>
+        <v-flex xs1 sm1 class="mt-3">
+          <v-avatar size="48px">
+            <img v-if="logged_user.img" :src="logged_user.img">
+            <v-icon v-else x-large>account_circle</v-icon>
+          </v-avatar>
+        </v-flex>
+        <v-flex xs9 sm10>
+          <v-text-field v-model="text" auto-grow rows="2" multi-line label="Adicione uma resposta" 
+            @focus="editing=true" @blur="losefocus" @keyup.ctrl.enter="send_reply(comment)"
+             hint="Ctrl+Enter para enviar" persistent-hint></v-text-field>
+          <p class="text-xs-right"><v-btn @click="send_reply(comment)" :loading="comment.sending_reply" primary>Enviar</v-btn></p>
         </v-flex>
       </template>
     </template>
+    <div ref="end"></div>
   </v-layout>
 </template>
 
@@ -47,6 +63,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import AppApi from '~apijs'
+import Toasts from '~/components/Toasts.js'
 
 export default {
   props: ['comments'],
@@ -71,10 +88,26 @@ export default {
         this.comments.push(response.data)
         this.sending = false;
         this.text = '';
+        setTimeout(()=>{
+          this.$refs.end.scrollIntoView()
+        }, 1)
+        
       })
     },
-    openreply() {
+    send_reply(comment){
 
+    },
+    losefocus(){
+      window.setTimeout(()=>{
+        this.editing = false;
+      }, 500)
+    },
+    openreply(comment) {
+      if (this.logged_user) {
+        Vue.set(comment, 'showreply', true);
+      } else {
+        Toasts.show('Faça login para responder', {timeout: 3000});
+      }
     }
   }
 }
